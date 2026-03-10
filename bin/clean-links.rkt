8ecf6ed5da2ef5c1f3dd4ae0031ca12d11e6430d
Bogdan Popa <bogdan@defn.io>
2026-03-10 07:32:29 +0200

    bin,prepare-distribution: reduce distro size by about 100MB

--- bin/clean-links.rkt
+++ bin/clean-links.rkt
@@ -8,6 +8,8 @@
    #:args [path]
    path))
 
+(define-values (dir _name _is-dir?)
+  (split-path links.rktd))
 (define links
   (call-with-input-file links.rktd read))
 (define filtered-links
@@ -15,7 +17,9 @@
    values
    (for/list ([l (in-list links)])
      (match l
-       [`(,_ (#"pkgs" ,_ ...)) l]
+       [`(,_ (#"pkgs" ,names ...))
+        (define pkg-path (apply build-path dir "pkgs" (map bytes->string/utf-8 names)))
+        (and (directory-exists? pkg-path) l)]
        [_ #f]))))
 (call-with-output-file links.rktd
   #:exists 'replace
